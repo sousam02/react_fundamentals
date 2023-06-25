0ecd3ebaa2b6be1a9c344ac9855cf04d5923d50d
sousam02 <moises.sousa583@gmail.com>
2023-06-25 11:09:51 -0300

    Configuring Babel, and generatinrating Bundle with Webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <h1 id='title'>
+        <span>Hello world!!!!!</span>
+    </h1>,
+    document.getElementById('root'),
+)
