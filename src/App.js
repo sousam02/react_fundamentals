85f5b603fae9c04929403b6ac12bcaebe03947f2
sousam02 <moises.sousa583@gmail.com>
2023-06-30 01:11:22 -0300

    useState: what is and how it works, and how to use callback functions with props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,67 @@
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import Header from "./Header";
 
 function App() {
+
+    const [posts, setPosts] = useState([
+        {
+            id: Math.random(),
+            title: "Title#01",
+            subtitle: "Subtitle#01",
+        },
+        {
+            id: Math.random(),
+            title: "Title#02",
+            subtitle: "Subtitle#02",
+        },
+        {
+            id: Math.random(),
+            title: "Title#03",
+            subtitle: "Subtitle#03",
+        }
+    ])
+
+    function handlePost() {
+        setPosts((prevState) => [
+            ...prevState,
+            {
+                id: Math.random(),
+                title: `Title#0${prevState.length + 1}`,
+                subtitle: `Subtitle#0${prevState.length + 1}`,
+            }
+        ])
+    }
+
+    function handleRemove(postId) {
+        setPosts((prevState) => (
+            prevState.filter((post) => post.id !== postId)
+        ));
+    }
+
     return (
         <>
             <Header title="JStack's Blog">
-                <h4>Week posts</h4>
+                <h4>
+                    Week posts
+                    <button onClick={handlePost}>Atualizar</button>    
+                </h4>
             </Header>
             <hr />
 
-            <Post
-                post={{
-                    title: "Title from article 1",
-                    subtitle: "Subtitle from article 1"
-                }}
-            />
-    
-            <Post
-                post={{
-                    title: "Title from article 2",
-                    subtitle: "Subtitle from article 2"
-                }}
-            />
-
-            <Post
-                post={{
-                    title: "Title from article 3",
-                    subtitle: "Subtitle from article 3"
-                }}
-            />
+            {
+                posts.map( post => (
+                    <Post
+                        onRemove={handleRemove}
+                        key={post.id}
+                        post={{
+                            id: post.id,
+                            title: post.title,
+                            subtitle: post.subtitle
+                        }}
+                    />
+                ))
+            }
         </>
     )
 }
--- src/Post.js
+++ src/Post.js
@@ -5,7 +5,9 @@ export default function Post(props) {
     return (
         <>
             <article>
-                <strong> {props.post.title} </strong><br/>
+                <strong> {props.post.title} </strong>
+                <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
+                <br/>
                 <small> {props.post.subtitle} </small>
             </article>
         </>
@@ -13,8 +15,11 @@ export default function Post(props) {
 }
 
 Post.propTypes = {
+    onRemove: PropTypes.func.isRequired,
     post:  PropTypes.shape({
+        id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
         subtitle: PropTypes.string.isRequired
     }).isRequired,
+
 }
